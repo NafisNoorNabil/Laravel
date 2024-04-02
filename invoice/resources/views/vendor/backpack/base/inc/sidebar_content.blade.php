af560f80623d4ac00b4fbeafddafc5639c67c900
Nafis Noor Nabil <61056516+NafisNoorNabil@users.noreply.github.com>
2024-04-02 10:24:37 +0600

    Delete invoice directory

--- invoice/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('customers') }}"><i class="nav-icon la la-question"></i> Customers</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('products') }}"><i class="nav-icon la la-question"></i> Products</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('invoices') }}"><i class="nav-icon la la-question"></i> Invoices</a></li>
